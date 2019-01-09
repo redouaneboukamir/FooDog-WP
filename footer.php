c9db81477ad39c18624d5f2fe1055ffe16653410
Valentin-Beaufays <beaufaysvalentin@gmail.com>
2019-01-09 09:48:03 +0100

    upload changes

--- footer.php
+++ footer.php
@@ -1,7 +1,3 @@
-    <script src="<?php bloginfo('template_url'); ?>/assets/js/jquery.min.js"></script>
-    <script src="<?php bloginfo('template_url'); ?>/assets/js/bootstrap.min.js"></script>
-    <script src="<?php bloginfo('template_url'); ?>/assets/js/script.js"></script>
     <?php wp_footer(); ?>
-    </div>
     </body>
 </html>
--- functions.php
+++ functions.php
@@ -8,4 +8,19 @@
         );
     }
     add_action( 'init', 'register_my_menus' );
+
+    //css files
+    function fooDog_enqueue_styles(){
+        wp_enqueue_style('css_fontawesome', get_template_directory_uri()."/assets/css/all.css");
+        wp_enqueue_style('css_bootstrap', get_template_directory_uri()."/assets/css/bootstrap.min.css");
+        wp_enqueue_style('css_style', get_template_directory_uri()."/style.css");
+    }
+    add_action('enqueue_styles', fooDog_enqueue_styles());
+    //js files
+    function fooDog_enqueue_scripts(){
+        wp_enqueue_script('js_jquery', get_template_directory_uri()."/assets/js/jquery.min.js");
+        wp_enqueue_script('js_bootstrap', get_template_directory_uri()."/assets/js/bootstrap.min.js");
+        wp_enqueue_script('js_script', get_template_directory_uri()."/assets/js/script.js");
+    }
+    add_action('enqueue_scripts', fooDog_enqueue_scripts());
 ?>
