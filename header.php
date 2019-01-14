1ac8cb57046d83b422cd32a06793b009e35a2126
Valentin-Beaufays <beaufaysvalentin@gmail.com>
2019-01-14 12:12:49 +0100

    responsive update

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
         <?php wp_nav_menu(array('theme_location' => 'fooDog_socialNavList', 'menu_class' => 'fooDog_socialNavList')); ?>
     </nav>
 </div>
-<div class="fooDog_content">
+<div class="fooDog_content_header">
     <header class="header">
         <div class="row">
             <div class="fooDog_title col-12">
@@ -29,10 +29,13 @@
             </div>
         </div>
         <div class="row">
-            <div class="fooDog_main col-12 ">
-                <nav class="fooDog_mainNav">
-                    <?php wp_nav_menu(array('theme_location' => 'fooDog_mainNavList', 'menu_class' => 'fooDog_mainNavList')); ?>
-                </nav>
-            </div>
+            <nav class="navbar navbar-expand-lg">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
+                </button>
+                <div class="fooDog_main collapse navbar-collapse" id="navbarSupportedContent">
+                    <?php wp_nav_menu(array('theme_location' => 'fooDog_mainNavList', 'menu_class' => 'fooDog_mainNavList navbar-nav mr-auto col-8 offset-2', 'container' => '')); ?>
+                </div>
+            </nav>
         </div>
     </header>
