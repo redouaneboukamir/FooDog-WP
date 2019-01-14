4bf4172646b5fb435a77baaf04d4fe14ffad08c3
redouane <redouaneboukamir@gmail.com>
2019-01-14 09:20:47 +0100

    save

--- functions.php
+++ functions.php
@@ -8,4 +8,7 @@
         );
     }
     add_action( 'init', 'register_my_menus' );
+    add_theme_support('post-thumbnails');
+    // $src = wp_get_attachment_image_src(get_post_thumbnail_id());
+    // add_image_size($small, 300,300);
 ?>
--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/style.css">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
@@ -39,8 +39,8 @@
             </div>
 
         <?php 
-        $before = "<h1>";
-        $after = "</h1>";
+        $before = '<h4 class="titleSticky">';
+        $after = "</h4>";
                 
             
                $recentPosts = new WP_Query();
@@ -53,14 +53,32 @@
                    );
                 $recentPosts->query($args);
                 $i = 0;
-                   while ( $i < 4 && $recentPosts->have_posts() ) : $recentPosts->the_post();	
+                ?>
+                <div class="headerPost row col-8">
+                            <h6 class="col-3">Featured posts</h6>
+                            <div class="borderHeaderPost col-9"></div>
+                </div>
+                <?php
+                   while ( $i < 3 && $recentPosts->have_posts() ) : $recentPosts->the_post();	
                    $i++;
                    ?>
-                   <div class="stickyPost">
-                
-                        <span class="categorySticky"><?php the_category();?> </span>
-                        <?php  the_title($before,$after);?>
-                        <?php the_content(); ?>
+                   <div class="contentStickyPost col-8">
+                       
+                        <div class="stickyPost row"> 
+                            <figure class="imageSticky col-5"><?php 
+                            if(has_post_thumbnail()){
+                                
+                                the_post_thumbnail();
+                            }
+                            
+                             ?></figure>
+                             <div class="contentArticleSticky col-7">
+                                <span class="categorySticky"><?php the_category();?> </span>
+                                <?php  the_title($before,$after);?>
+                                <?php the_content(); ?>
+                                <span></span>
+                            </div>
+                        </div>
 
                </div>
               <?php endwhile;?>
