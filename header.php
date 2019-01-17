63391ee7d0980b00ad8672c5ebdcb6f21551f813
redouane <redouaneboukamir@gmail.com>
2019-01-17 11:46:41 +0100

    save

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>style.css">
 
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
