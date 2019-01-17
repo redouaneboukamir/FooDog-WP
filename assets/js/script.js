82fea6bbd249127b35e61a9e2229a929a40b6026
Maxime Christiaens <43206422+Maxime-Christiaens@users.noreply.github.com>
2019-01-17 16:06:43 +0100

    supprime all

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-let popup = document.getElementById('fooDog_pop_up');
-function search_pop_up(){
-    document.getElementById('fooDog_pop_up').style.display="block";
-}
-
-function hide_pop_up(){
-    document.getElementById('fooDog_pop_up').style.display="none";
-}
