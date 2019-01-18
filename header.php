53f98fc4588af44ee09f27e394895a8968c92772
redouane <redouaneboukamir@gmail.com>
2019-01-18 09:50:12 +0100

    s

--- header.php
+++ header.php
@@ -9,12 +9,12 @@
 
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>style.css">
     <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
-
+    
     <title><?php wp_title(); ?></title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>style.css">
     <?php wp_head(); ?>
 </head>
 <body>
