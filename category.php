816d4361cc6843ffe06aeb5941c06cac812a0644
Maxime Christiaens <43206422+Maxime-Christiaens@users.noreply.github.com>
2019-01-18 16:25:02 +0100

    Mise en place de la pagination

--- category.php
+++ category.php
@@ -1,17 +1,23 @@
 <?php get_header();?>
 
 <!-- <div class="contentAllAll"> -->
-
-    <span class="titleCat col-12"><h2><?php single_cat_title();?></h2></span>
-    <div class="contentAllCategoryPub container">
-
 <div class="contentAllCategoryPub container">
-
         <div class="contentAllCategoryPost col-8">
     <?php
-    if (have_posts()) :
-            while (have_posts()) : the_post(); 
-            ?>
+    //setup pour la pagination avec WP_Query (permet aussi de configurer les articles à afficher comme l'ordre, le nombre d'article maximum, ect...)
+    $pageCat = get_query_var("paged");
+
+    $CategoryPost_args = array (
+        "posts_per_page" => 8,
+        "paged" => $pageCat,
+        "orderby" => "date",
+        //"category_name" => the_category(),
+        "order"   => "DESC"
+    );
+    $CategoryPost = new WP_Query($CategoryPost_args);
+    //fin de setup de la pagination 
+    if ($CategoryPost->have_posts()) : while ($CategoryPost->have_posts()) : $CategoryPost->the_post(); 
+    ?>
         <!-- Ensemble du traitement et affichage de stickypost -->
         <?php 
                     $before = '<h4 class="titleCategory">';
@@ -60,13 +66,24 @@
                         </div>
                         <figure><img src=""></figure>
                     </div>
-
                     <div class="nav-previous alignleft"><?php previous_posts_link( 'Older posts' ); ?></div>
                     <div class="nav-next alignright"><?php next_posts_link( 'Newer posts' ); ?></div>  
     </div>          
     <?php endif;?>
+    <div class="contentPagination col-lg-8">
+        <span class="pagination col-12">
+            <?php
+            //pagination 
+            //$paginationCat créer les arguments
+        $paginationCat = array(
+            "prev_text" => __("<"),
+            "next_text" => __(">"),
+            );
+            //echo la pagination avec les arguments setup préalablement
+            echo paginate_links($paginationCat);
+            ?>
 
+        </span>
 <?php get_footer(); ?>
-
     <!-- <div class="col-2">fzesv</div> -->
 <!-- </div> -->                      
