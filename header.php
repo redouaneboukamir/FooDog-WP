0b006871c88a3f83aec5d5ae53fc9e2b78e76e6a
Valentin-Beaufays <beaufaysvalentin@gmail.com>
2019-01-18 10:38:55 +0100

    fix social menu

--- header.php
+++ header.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>style.css">
     <link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
@@ -16,25 +13,16 @@
 <body>
 <!-- search pop-up-->
 <div class="fooDog_pop_up">
-
+<div class="fooDog_searchWindow">
+   
+   </div>
 </div>
 <!-- page layout -->
 <div class="fooDog_social col-12">    
     <nav class="fooDog_socialNav debug">
         <?php 
-        wp_nav_menu(array('theme_location' => 'fooDog_socialNavList', 'menu_class' => 'fooDog_socialNavList debug')); 
+            wp_nav_menu(array('theme_location' => 'fooDog_socialNavList', 'menu_class' => 'fooDog_socialNavList debug')); 
         ?>
-
-    <div class="fooDog_searchWindow">
-   
-    </div>
-</div>
-<!-- page layout -->
-<div class="fooDog_social col-12">    
-    <nav class="fooDog_socialNav">
-        <?php wp_nav_menu(array('theme_location' => 'fooDog_socialNavList', 'menu_class' => 'fooDog_socialNavList')); ?>
-
-    </nav>
 </div>
 <div class="fooDog_content_header">
     <header class="header">
