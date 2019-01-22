3d063219c4f5ab937bd24324c590f920ccac2ba4
Maxime Christiaens <maxime.ad.christiaens@gmail.com>
2019-01-22 08:38:33 +0100

    push

--- category.php
+++ category.php
@@ -18,63 +18,61 @@ $CategoryPost = new WP_Query($CategoryPost_args);
 //fin de setup de la pagination
 if ($CategoryPost->have_posts()): while ($CategoryPost->have_posts()): $CategoryPost->the_post();
         ?>
-										        <!-- Ensemble du traitement et affichage de stickypost -->
-										        <?php
-        $before = '<h4 class="titleCategory">';
-        $after = "</h4>";
+<!-- Ensemble du traitement et affichage de stickypost -->
+<?php
+$before = '<h4 class="titleCategory">';
+$after = "</h4>";
         ?>
-										                            <div class="contentCategoryPost ">
-										                                    <div class="categoryPost row">
-										                                        <figure class="imageCategory col-12 col-md-5">
-				<?php
-        if (has_post_thumbnail()) {
-
-            the_post_thumbnail();
-        } else { //Place automatiquement un thumbnail, si aucun ne sont présent
+		<div class="contentCategoryPost ">
+			<div class="categoryPost row">
+				<figure class="imageCategory col-12 col-md-5">
+<?php
+if (has_post_thumbnail()) {	
+	the_post_thumbnail();
+} else { //Place automatiquement un thumbnail, si aucun ne sont présent
             echo ('<img src="https://news.nationalgeographic.com/content/dam/news/2018/05/29/dog-baby-talk/01-dog-baby-talk-NationalGeographic_2283205.ngsversion.1527786004161.adapt.1900.1.jpg" alt="">');
         }
-
-        ?></figure>
-										                                        <div class="contentArticleCategory col-12 col-md-7">
-										                                            <span class="categoryCategory"><?php the_category();?> </span>
-																					<a class="pubCategory" href="<?php the_permalink()?>"><?php the_title($before, $after);?></a>
-
-						<?php
+?>
+		</figure>
+			<div class="contentArticleCategory col-12 col-md-7">
+				<span class="categoryCategory"><?php the_category();?> </span>
+				<a class="pubCategory" href="<?php the_permalink()?>"><?php the_title($before, $after);?></a>
+<?php
         $more = 30;
         $aftermore = false;
         // the_content($more,$aftermore);
         the_excerpt();
         ?>
-						<div class="contentShareCat">
-								                                        <span class="shareIconCat"><i class="fas fa-share"></i></span>
-								                                        <span class="shareTextCat">Share</span>
-								                                    </div>
-										                                        </div>
-										                                    </div>
-										                            </div>
-										                            <?php endwhile;?>
-					                        </div>
-					                        <div class="pubCategory col-4 col-md-4">
-					                        <div class="contentSociaux col-12">
-					                            <div class="facebookSlide col-12">
-					                            <i class="fab fa-facebook-f"></i>
-					                                <span>Facebook</span>
-					                            </div>
-					                            <div class="twitterSlide col-12">
-					                            <i class="fab fa-twitter"></i>
-					                                <span>Twitter</span>
-					                            </div>
-					                            <div class="instaSlide col-12">
-					                            <i class="fab fa-instagram"></i>
-					                                    <span>Instagram</span>
-												</div>
-												<div class="col-12">
-													<img class="img-fluid" src="https://www.wanimo.com/images/alimentation_pour_chien/A/00/A00EK07/500x500/01/royal-canin-breed-nutrition-shih-tzu.jpg" alt="Pub pour de la nourrture pour chien">
-												</div>
-					                        </div>
-					                    </div>
-					    </div>
-					    <?php endif;?>
+				<div class="contentShareCat">
+					<span class="shareIconCat"><i class="fas fa-share"></i></span>
+					<span class="shareTextCat">Share</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php endwhile;?>
+	</div>
+	<div class="pubCategory col-4 col-md-4">
+		<div class="contentSociaux col-12">
+			<div class="facebookSlide col-12">
+				<i class="fab fa-facebook-f"></i>
+				<span>Facebook</span>
+			</div>
+			<div class="twitterSlide col-12">
+				<i class="fab fa-twitter"></i>
+				<span>Twitter</span>
+			</div>
+			<div class="instaSlide col-12">
+				<i class="fab fa-instagram"></i>
+				<span>Instagram</span>
+			</div>
+			<div class="col-12">
+				<img class="img-fluid" src="https://www.wanimo.com/images/alimentation_pour_chien/A/00/A00EK07/500x500/01/royal-canin-breed-nutrition-shih-tzu.jpg" alt="Pub pour de la nourrture pour chien">
+			</div>
+		</div>
+	</div>
+</div>
+<?php endif;?>
     <div class="contentPagination col-lg-8">
         <span class="pagination col-12">
             <?php
